c7d97d6e7db3c4b9b89a79f91f6e86d640a2c98e
vanshikka-sharma <168462507+vanshikka-sharma@users.noreply.github.com>
2026-09-04 00:39:16 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        // Check rows
+        for(int i = 0; i < 9; i++) {
+            set<char> s;
+
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.') continue;
+
+                if(s.count(board[i][j]))
+                    return false;
+
+                s.insert(board[i][j]);
+            }
+        }
+
+        // Check columns
+        for(int j = 0; j < 9; j++) {
+            set<char> s;
+
+            for(int i = 0; i < 9; i++) {
+                if(board[i][j] == '.') continue;
+
+                if(s.count(board[i][j]))
+                    return false;
+
+                s.insert(board[i][j]);
+            }
+        }
+
+        // Check 3x3 boxes
+        for(int row = 0; row < 9; row += 3) {
+            for(int col = 0; col < 9; col += 3) {
+
+                set<char> s;
+
+                for(int i = row; i < row + 3; i++) {
+                    for(int j = col; j < col + 3; j++) {
+
+                        if(board[i][j] == '.') continue;
+
+                        if(s.count(board[i][j]))
+                            return false;
+
+                        s.insert(board[i][j]);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
