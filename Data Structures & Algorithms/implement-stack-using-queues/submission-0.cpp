0c8f0205639244e9d469ca15a83c9800efe08aaf
vanshikka-sharma <168462507+vanshikka-sharma@users.noreply.github.com>
2026-09-25 10:10:12 +0530

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.cpp
@@ -0,0 +1,39 @@
+class MyStack {
+public:
+    stack<int> st;
+    MyStack() {
+
+    }
+    
+    void push(int x) {
+        st.push(x);
+    }
+    
+    int pop() {
+        int x = st.top();
+        st.pop();
+        return x;
+    }
+    
+    int top() {
+        int x = st.top();
+        return x;
+    }
+    
+    bool empty() {
+        if(st.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
